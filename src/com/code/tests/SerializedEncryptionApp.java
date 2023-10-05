46185da5d5dcf1e0cbbd18e40f7a6e3ef8d8d029
Gabriel Vasconez <118301395+Gabo0526@users.noreply.github.com>
2023-10-05 10:12:00 -0500

    update

--- src/com/code/tests/SerializedEncryptionApp.java
+++ src/com/code/tests/SerializedEncryptionApp.java
@@ -9,15 +9,15 @@ public class SerializedEncryptionApp {
         SerializedEncryption encryption = new SerializedEncryption();
         ByteArrayOutputStream ciphertext = encryption.encryptNewText("Hola, mundo!", 13);
 
-        System.out.printf("El texto cifrado es: %s\n", ciphertext);
+        System.out.printf("El texto cifrado y serializado es: %s\n", ciphertext);
 
         SerializedDecryption decryption = new SerializedDecryption(ciphertext, 13);
-        System.out.printf("El texto descifrado es: %s\n\n", decryption.getDecipheredText());
+        System.out.printf("El texto descifrado y deserializado es: %s\n\n", decryption.getDecipheredText());
 
         ciphertext = encryption.encryptNewText("Hey! Este es un nuevo texto", 12435);
-        System.out.printf("El texto cifrado es: %s\n", ciphertext);
+        System.out.printf("El texto cifrado y serializado es: %s\n", ciphertext);
 
         decryption.decipherNewText(ciphertext, 12435);
-        System.out.printf("El texto descifrado es: %s\n", decryption.getDecipheredText());
+        System.out.printf("El texto descifrado y deserializado es: %s\n", decryption.getDecipheredText());
     }
 }
